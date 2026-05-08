df19a325cc0eef7c9ff89c6c54c90b22f605dee3
Henil Suthar <sutharhenil3304@gmail.com>
2026-05-08 14:17:06 +0530

    feature: add users entity

---
+++ backend/src/main/java/com/todo/backend/entity/User.java
@@ -0,0 +1,26 @@
+package com.todo.backend.entity;
+
+import jakarta.persistence.*;
+import lombok.Data;
+
+import java.time.LocalDateTime;
+
+@Data
+@Entity
+@Table(name = "users")
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(nullable = false,unique = true)
+    private String email;
+
+    @Column(nullable = false)
+    private String password;
+
+    private LocalDateTime created_at;
+}
